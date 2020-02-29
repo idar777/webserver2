39e91c60d2378f9e6965ffb8f2f154b1b27d7f21
Idar777 <aydar.abdulkhakov@gmail.com>
2020-02-29 22:22:15 +0300

    first try

--- home/box/web/hello.py
+++ home/box/web/hello.py
@@ -1,4 +1,6 @@
-test
-
-
-sd;lfksdlf
+def hello(environ, start_response): # бизнес-логика
+   status = '200 OK'
+   headers = [('Content-Type', 'text/plain') ]
+   getParam = environ['QUERY_STRING'].replace('&', '\n')
+   start_response(status, headers)
+   return [ getParam ]
---
+++ home/box/web/init.sh
@@ -0,0 +1,3 @@
+sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo gunicorn -b 0.0.0.0:8080 hello
